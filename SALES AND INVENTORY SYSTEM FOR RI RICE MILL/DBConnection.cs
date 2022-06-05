77e8afefc4205928da9d4628497a67e71393fa1b
Sherynhell Ann Perales <92663371+Sherynhdipity@users.noreply.github.com>
2022-06-06 01:47:58 +0800

    connection string

--- SALES AND INVENTORY SYSTEM FOR RI RICE MILL/DBConnection.cs
+++ SALES AND INVENTORY SYSTEM FOR RI RICE MILL/DBConnection.cs
@@ -8,6 +8,6 @@ namespace SALES_AND_INVENTORY_SYSTEM_FOR_RI_RICE_MILL
 {
     class DBConnection
     {
-        public static string con = @"Data Source=MAVERICK-PC\SQLEXPRESS;Initial Catalog=DBSaleInventory;Integrated Security=True";
+        public static string con = @"Data Source=.\SQLEXPRESS;Initial Catalog=DBSaleInventory;Integrated Security=True";
     }
 }
